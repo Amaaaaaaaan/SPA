9e8f0812a3980f91675835277557253fa75d17a3
Aman Tomar <amantoma700@gmail.com>
2026-04-09 22:00:31 +0530

    Implement stopwatch feature using useRef hook

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,47 @@
 // export default App
 
 
+// stop watch
 
+import React, { useRef } from "react";
+
+function App() {
+  const timeRef = useRef(null);
+  const intervalRef = useRef(null);
+
+  let count = 0;
+
+  const start = () => {
+    if (intervalRef.current) return;
+
+    intervalRef.current = setInterval(() => {
+      count++;
+      timeRef.current.innerText = count;
+    }, 1000);
+  };
+
+  const stop = () => {
+    clearInterval(intervalRef.current);
+    intervalRef.current = null;
+  };
+
+  const reset = () => {
+    stop();
+    count = 0;
+    timeRef.current.innerText = 0;
+  };
+
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h2 ref={timeRef}>0</h2>
+
+      <button onClick={start}>Start</button>
+      <button onClick={stop}>Stop</button>
+      <button onClick={reset}>Reset</button>
+    </div>
+  );
+}
+
+export default App;
 
 
