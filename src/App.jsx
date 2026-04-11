ee22b0324563462501497a6f5bbb16635ead6d11
Aman Tomar <amantoma700@gmail.com>
2026-04-11 16:43:40 +0530

    usage of contextAPI

--- src/App.jsx
+++ src/App.jsx
@@ -15,46 +15,79 @@
 
 // export default App
 
-// light bulb functionality
 
-import { useState,useEffect } from "react";
 
-function App() {
 
-return <div>
-  <LightBulb />
-</div>
-}
 
-function LightBulb() {
-  const [bulbOn, setBulbOn] = useState(true);  
-  return <div>
-    <BulbState bulbOn={bulbOn} />
-    <ToggleBulbState setBulbOn={setBulbOn} />
-  </div>
-}
 
-function BulbState({ bulbOn }) {
-  return <div>
-    {bulbOn ? <h1>Bulb is ON</h1> : <h1>Bulb is OFF</h1>}
-  </div>
+//* light bulb functionality {rollup of useState and useEffect hooks}//*
 
-}
+// import { useState,useEffect } from "react";
 
-function ToggleBulbState({ setBulbOn }) {
+// function App() {
+
+// return <div>
+//   <LightBulb />
+// </div>
+// }
+
+// function LightBulb() {
+//   const [bulbOn, setBulbOn] = useState(true);  
+//   return <div>
+//     <BulbState bulbOn={bulbOn} />
+//     <ToggleBulbState setBulbOn={setBulbOn} />
+//   </div>
+// }
+
+// function BulbState({ bulbOn }) {
+//   return <div>
+//     {bulbOn ? <h1>Bulb is ON</h1> : <h1>Bulb is OFF</h1>}
+//   </div>
+
+// }
+
+// function ToggleBulbState({ setBulbOn }) {
   
-  function toggle(){
-    setBulbOn(currentState => !currentState);
-  }
+//   function toggle(){
+//     setBulbOn(currentState => !currentState);
+//   }
 
-  return <div>
-    <button onClick={toggle}>Toggle the Bulb</button>
-  </div>
-}
+//   return <div>
+//     <button onClick={toggle}>Toggle the Bulb</button>
+//   </div>
+// }
 
-
-
-export default App
+// export default App
 
 
+// * working of useContext hook *//
 
+// import { createContext, useContext } from "react";
+
+// const UserContext = createContext();
+
+// function App() {
+//   return (
+//     <UserContext.Provider value="Aman">
+//       <Child />
+//     </UserContext.Provider>
+//   );
+// }
+
+// function Child() {
+//   const user = useContext(UserContext);
+//   return (
+//   <div>
+   
+//     <h1>Hello {user}</h1>
+//      <Childish/>
+//   </div>
+//   )
+// }
+
+// function Childish() {
+//   const user = useContext(UserContext);
+//   return <h1>Childish component says Hello : {user}</h1>;
+// }
+
+// export default App;
